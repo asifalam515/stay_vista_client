537a3551520da3cd34ba27d4858496f2cb84ab57
asifalam515 <asibulalam515@gmail.com>
2024-12-07 09:47:27 +0600

    started

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFtOsLQfoIamXFlXUPmNzys6_fXE0ac_8",
+  authDomain: "stayvista-81943.firebaseapp.com",
+  projectId: "stayvista-81943",
+  storageBucket: "stayvista-81943.firebasestorage.app",
+  messagingSenderId: "148838637346",
+  appId: "1:148838637346:web:98e430670d906826b78f7b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
