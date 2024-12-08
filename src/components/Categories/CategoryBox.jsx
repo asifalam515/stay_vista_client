988922469ef115963f3934aa693d68abe1646440
asifalam515 <asibulalam515@gmail.com>
2024-12-08 10:03:14 +0600

    query string category started

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,8 +1,12 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const handleClick = () => {
+    let currentQuery = { category: label };
+  };
   return (
     <div
+      onClick={() => handleClick}
       className={`flex 
   flex-col 
   items-center 
@@ -15,14 +19,14 @@ const CategoryBox = ({ label, icon: Icon }) => {
   cursor-pointer`}
     >
       <Icon size={26} />
-      <div className='text-sm font-medium'>{label}</div>
+      <div className="text-sm font-medium">{label}</div>
     </div>
-  )
-}
+  );
+};
 
 CategoryBox.propTypes = {
   label: PropTypes.string,
   icon: PropTypes.elementType,
-}
+};
 
-export default CategoryBox
+export default CategoryBox;
