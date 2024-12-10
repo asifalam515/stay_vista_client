9f78b16242f0e18755c1ab9cb0581366797a8ad4
asifalam515 <asibulalam515@gmail.com>
2024-12-10 09:47:48 +0600

    add room form created

--- src/pages/Dashboard/Host/AddRoom.jsx
+++ src/pages/Dashboard/Host/AddRoom.jsx
@@ -1,8 +1,11 @@
+import AddRoomForm from "../../../components/Form/AddRoomForm";
+
 const AddRoom = () => {
   return (
     <div>
       <h1 className="text-5xl">Add room page</h1>
       {/* render a form */}
+      <AddRoomForm></AddRoomForm>
     </div>
   );
 };
