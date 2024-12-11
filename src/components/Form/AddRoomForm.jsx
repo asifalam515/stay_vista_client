c94b2fab4e7b9f3aa2ab3031e3812a5213b41b2f
asifalam515 <asibulalam515@gmail.com>
2024-12-11 20:43:09 +0600

    updated

--- src/components/Form/AddRoomForm.jsx
+++ src/components/Form/AddRoomForm.jsx
@@ -2,7 +2,7 @@ import { categories } from "../Categories/CategoriesData";
 import { DateRange } from "react-date-range";
 import { useState } from "react";
 
-const AddRoomForm = ({ dates, handleDates }) => {
+const AddRoomForm = ({ dates, handleDates, handleSubmit }) => {
   const [state, setState] = useState([
     {
       startDate: new Date(),
@@ -12,7 +12,7 @@ const AddRoomForm = ({ dates, handleDates }) => {
   ]);
   return (
     <div className="w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
           <div className="space-y-6">
             <div className="space-y-1 text-sm">
--- src/pages/Dashboard/Host/AddRoom.jsx
+++ src/pages/Dashboard/Host/AddRoom.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import AddRoomForm from "../../../components/Form/AddRoomForm";
 import { setDate } from "date-fns";
-
+import useAuth from "../../../hooks/useAuth";
+import Swal from "sweetalert2";
 const AddRoom = () => {
+  const { user } = useAuth();
   const [dates, setDates] = useState({
     startDate: new Date(),
     endDate: null,
@@ -13,11 +15,52 @@ const AddRoom = () => {
     console.log(item);
     setDates(item.selection);
   };
+
+  //   form handler
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const location = form.location.value;
+    const category = form.category.value;
+    const title = form.title.value;
+    const to = "";
+    const from = "";
+    const price = form.price.value;
+    const guests = form.total_guest.value;
+    const bathrooms = form.bathrooms.value;
+    const description = form.description.value;
+    const bedrooms = form.bedrooms.value;
+    const image = form.image.files[0];
+    const host = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+    };
+    const image_url = "function";
+    Swal.fire({
+      title: "Do you want to save the changes?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Save",
+      denyButtonText: `Don't save`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Saved!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
+  };
   return (
     <div>
       <h1 className="text-5xl">Add room page</h1>
       {/* render a form */}
-      <AddRoomForm dates={dates} handleDates={handleDates}></AddRoomForm>
+      <AddRoomForm
+        dates={dates}
+        handleDates={handleDates}
+        handleSubmit={handleSubmit}
+      ></AddRoomForm>
     </div>
   );
 };
